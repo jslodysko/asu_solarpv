d22665beacad1d7c32a1fc7b473d8489256faaac
jslodysko <79119655+jslodysko@users.noreply.github.com>
2021-04-05 21:22:59 -0400

    Add files via upload

--- SolarPV/settings.py
+++ SolarPV/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,20 @@ WSGI_APPLICATION = 'SolarPV.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ift458-d7',
+        'USER': 'root',
+        'PASSWORD': 'Secret11',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
